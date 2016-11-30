3f34264a7af0d61cbe4abada1b07c1a5ecec2a09
M. Medrano Johan <medrano@ibm.insa-toulouse.fr>
2016-11-30 19:59:13 +0100

    Rvc - Tsm OK

--- TP_Ethernet_2/Test_core.vhd
+++ TP_Ethernet_2/Test_core.vhd
@@ -42,7 +42,7 @@ ARCHITECTURE behavior OF Receiver_Test IS
     COMPONENT Core
     PORT(
          RBYTEP : OUT  std_logic;
-			CLKDIV8_UP : out  STD_LOGIC;
+			CLKDIV8_UP : out  STD_LOGIC;	
          RCLEANP : OUT  std_logic;
          RCVNGP : OUT  std_logic;
          RDATAO : OUT  std_logic_vector(7 downto 0);
@@ -151,12 +151,12 @@ BEGIN
 
       -- insert stimulus here 
 		
---		RDATAI <= (others=>'1') after 200ns; 
---		RDATAI <= X"AB" after 400ns; 
---		RENABP <= '0', '1' after 600ns; 
---		RDATAI <= X"AB"; 
---		RENABP <= '1'; 
---		RDATAI <= X"12" after 80ns, X"34" after 2*80ns, X"56" after 3*80ns, X"12" after 4*80ns,X"34" after 5*80ns,X"56" after 6*80ns,X"AB" after 10*80ns,X"00" after 11*80ns,X"AB" after 12*80ns- 20ns,X"12" after 13*80ns- 20ns,X"34" after 14*80ns- 20ns,X"56" after 15*80ns- 20ns,X"AB" after 16*80ns- 20ns  ;
+		RDATAI <= (others=>'1') after 200ns; 
+		RDATAI <= X"AB" after 400ns; 
+		RENABP <= '0', '1' after 600ns; 
+		RDATAI <= X"AB"; 
+		RENABP <= '1'; 
+		RDATAI <= X"12" after 80ns, X"34" after 2*80ns, X"56" after 3*80ns;--, X"12" after 4*80ns,X"34" after 5*80ns,X"56" after 6*80ns,X"AB" after 10*80ns,X"00" after 11*80ns,X"AB" after 12*80ns- 20ns,X"12" after 13*80ns- 20ns,X"34" after 14*80ns- 20ns,X"56" after 15*80ns- 20ns,X"AB" after 16*80ns- 20ns  ;
 	
 		TAVAILP <= '0', '1' after 200ns, '0' after 210ns; 
 		TDATAI <= X"12" after 80ns, X"34" after 2*80ns, X"56" after 3*80ns, X"12" after 4*80ns,X"34" after 5*80ns;

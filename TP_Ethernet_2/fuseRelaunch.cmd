7f94dafe55a9b919ea912dd68d0b275bbe8a7fad
Johan Medrano <jmsnowrider@gmail.com>
2016-12-02 19:37:17 +0100

    Final behavioral working version

--- TP_Ethernet_2/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "/home/yop/Programmation/VHDL/Ethernet/TP_Ethernet_2/Receiver_Test_isim_beh.exe" -prj "/home/yop/Programmation/VHDL/Ethernet/TP_Ethernet_2/Receiver_Test_beh.prj" "work.Receiver_Test" 
